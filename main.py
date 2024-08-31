bf6745321e670b4e8ba856ce65b9f2e7f54085ec
Олександр Швайгер <sasasvajger38@gmail.com>
2024-08-31 14:43:52 +0300

    second commit

--- main.py
+++ main.py
@@ -3,40 +3,44 @@
 
 import datetime
 
-def get_days_from_today(date):
+def get_days_from_today(date='2023-10-09'):
 
-    today_date = datetime.datetime(year=2024, month=8, day=14)
-    future_date = datetime.datetime(year=2024, month=10, day=10)
-
-    difference_date = future_date - today_date
-
-    print(difference_date)
-
+    gotten_date = datetime.date(date)
+    current_date = datetime.date.today()
+    difference_date = current_date - gotten_date
+    return date
 
 # ДРУГЕ ЗАВДАННЯ
 
 import random
 
-def get_numbers_ticket(min, max, quantity):
+def get_numbers_ticket(min=1, max=49, quantity=6):
+    min = input("Enter any number from 1 to 10: ")
+    min = int(min)
 
-    number_tickets = (3, 16, 34, 48, 22, 8)
-    lottery_numbers = random.choices(number_tickets, k=1)
-    print("Your lottery number is...", lottery_numbers)
+    max = input("Enter maximum number not more than 1000: ")
+    max = int(max)
+
+    quantity = input("Enter quantity of numbers you want to obtain: ")
+    quantity = int(quantity)
+
+    return min, max, quantity
+
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
+
 
 # ТРЕТЄ ЗАВДАННЯ
 
 import re
 
-def normalize_phone(phone_number):
-
-    mobilize_phone_numbers = [
+mobilize_phone_numbers = [
     "+380501233234",
     "+380503451234",
     "+380508889900",
     "+380501112222",
     "+380501112211"
-    ]
+]
 
-    sanitized_phone_number = [mobilize_phone_numbers]
-    print("Normalize phone numbers for SMS:", sanitized_phone_number)
-
+sanitized_phone_number = [mobilize_phone_numbers]
+print("Normalize phone numbers for SMS:", sanitized_phone_number)
