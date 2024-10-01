00cf0eb462bb078131d628642f8ed938a7de0c77
Олександр Швайгер <sasasvajger38@gmail.com>
2024-10-01 18:10:13 +0300

    commit

--- goit-algo-hw-05.py
+++ goit-algo-hw-05.py
@@ -23,9 +23,17 @@ print(fib(15))
 
 # ЗАВДАННЯ 2. МОДУЛЬ 5
 
+from typing import Callable
+
 def generator_numbers(text: str):
-    
+    numbers = "1000.0", "27.45", "324.00"
+    for number in numbers:
+        yield number
+
+def sum_profit(text: str, func: Callable):
+    yield sum(text, func)
 
 
-# ЗАВДАННЯ 4. МОДУЛЬ 5
-
+text = generator_numbers(text="Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів.")
+total_income = sum_profit(text, generator_numbers)
+print(f"Загальний дохід: {total_income}")
