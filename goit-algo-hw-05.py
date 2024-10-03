9719a7b4e5cf268cba6dbec5f13fc267086f5e8d
Олександр Швайгер <sasasvajger38@gmail.com>
2024-10-03 11:09:45 +0300

    commit

--- goit-algo-hw-05.py
+++ goit-algo-hw-05.py
@@ -21,21 +21,73 @@ print(fib(10))
 print(fib(15))
 
 
-# ЗАВДАННЯ 2. МОДУЛЬ 5
+# ЗАВДАННЯ 4. МОДУЛЬ 5
 
-import re
+contacts = {}
 
-from typing import Callable
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            return "Give me name and phone please."
 
-def generator_numbers(text: str):
-    numbers = re.findall(r'\d+\.\d+|\d+', text)
-    for number in numbers:
-        yield number
+    return inner
 
-def sum_profit(text: str, func: Callable):
-    yield sum(text(func))
+@input_error
+def add_contact(args):
+    if len(args) == 2:
+        name, phone = args
+        contacts[name] = phone
+        return "Contact added."
+    else:
+        return "Enter the argument for the command."
 
+@input_error
+def show_phone(args):
+    if len(args) == 1:
+        name = args[0]
+        if name in contacts:
+            return contacts[name]
+        else:
+            return "Contact not found."
+    else:
+        return "Enter the argument for the command."
 
-text = generator_numbers(text="Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів.")
-total_income = sum_profit(text, generator_numbers)
-print(f"Загальний дохід: {total_income}")
+@input_error
+def show_all(*args, **kwargs):
+    if contacts:
+        return "\n".join([f"{name.format()}: {phone}" for name, phone in contacts.items()])
+    else:
+        return "Enter the argument for the command."
+
+@input_error
+def parse_input(user_input):
+    parts = user_input.lower().split()
+    command = parts[0]
+    args = parts[1:]
+    return command, args
+
+def main():
+    while True:
+        user_input = input("Enter a command: ")
+        command, args = parse_input(user_input)
+
+        if command == "add":
+            print(add_contact(args))
+        
+        elif command == "phone":
+            print(show_phone(args))
+        
+        elif command == "all":
+            print(show_all(args))
+        
+        elif command in ["close", "exit"]:
+            print("Good bye!")
+            break
+        
+        else:
+            print("Unknown command. Please try again.")
+
+if __name__ == "__main__":
+    main()
