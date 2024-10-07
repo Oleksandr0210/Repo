21bb087b006edd9782cf994877da3fe84d5a404b
Олександр Швайгер <sasasvajger38@gmail.com>
2024-10-07 14:30:53 +0300

    commit

--- goit-algo-hw-06.py
+++ goit-algo-hw-06.py
@@ -14,6 +14,9 @@ class Name(Field):
     def __init__(self, name):
         self.name = name
 
+    def __init__(self, value):
+        self.value = value
+
 class Phone(Field):
     def __init__(self, phone):
         self.phone = phone
@@ -23,21 +26,37 @@ class Record:
         self.name = Name(name)
         self.phones = []
 
-    def __str__(self):
-        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
-
     def add_phone(self, phone):
         return phone
+
+    def remove_phone(self, phone):
+        return phone
+
+    def edit_phone(self, phone):
+        return phone
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
 
     def add_record(self, record):
         return record
 
 class AddressBook(UserDict):
     def add_record(self, record):
-        return record
+        self.data = record
 
     def find(self, name):
+        return Record(name)
+
+    def delete(self, name):
         return name
+
+class Phone(Field):
+    def __init__(self, phone):
+        self.phone = phone
+
+    def find_phone(self):
+        pass
 
 
 book = AddressBook()
@@ -47,13 +66,16 @@ john_record.add_phone("5555555555")
 book.add_record(john_record)
 jane_record = Record("Jane")    
 jane_record.add_phone("9876543210")
+jane_record.add_phone("0987654342")
 book.add_record(jane_record)
 print(book)
 
 john = book.find("John")
-john.edit_phone("1234567890", "1112223333")
+jane = book.find("Jane")
 print(john)
+print(jane)
 
-found_phone = john.find_phone("5555555555")
+found_phone = john.add_phone("5555555555")
 print(f"{john.name}: {found_phone}")
-book.delete("Jane")
+found_phone = jane.add_phone("1234567890")
+print(f"{jane.name}: {found_phone}")
